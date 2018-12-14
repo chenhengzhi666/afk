89bc2d392fff0f6681ce925e80962f8f3615f4c1
chenhengzhi <hengzhi.chen@foxmail.com>
2018-12-14 18:05:58 +0800

    afk开发

---
+++ js/award1.js
@@ -0,0 +1,289 @@
+$(function () {
+    let turnplate = {
+        restaraunts: [], //大转盘奖品名称
+        colors: [], //大转盘奖品区块对应背景颜色
+        outsideRadius: 222, //大转盘外圆的半径
+        textRadius: 165, //大转盘奖品位置距离圆心的距离
+        insideRadius: 65, //大转盘内圆的半径
+        startAngle: 0, //开始角度
+        bRotate: false, //false:停止;ture:旋转
+    };
+    let openid = '';
+    let nickname = '';
+    let count = 0;
+    let countMax = 1; //单人可玩次数
+    let gameState = 1; //0：活动暂未开放	1：活动正常开发
+    let activityStart = '2018-11-23'; //活动开始时间
+    let activityEnd = '2018-12-23'; //活动结束时间
+
+    let getQueryString = name => {
+        let reg = new RegExp(`(^|&)${name}=([^&]*)(&|$)`);
+        let r = window.location.search.substr(1).match(reg);
+        return r != null ? unescape(r[2]) : r;
+    }
+
+    let init = () => {
+        mui.showLoading("正在加载..", "div"); //加载文字和类型，plus环境中类型为div时强制以div方式显示
+        if (getQueryString('code')) {
+            // 用户信息已获取
+            // 用户再次刷新时判断localStorage是否用户信息已获取
+            if (localStorage.getItem('openid')) {
+                openid = localStorage.getItem('openid');
+                nickname = localStorage.getItem('nickname');
+            } else {
+                $.get('/getUserInfo', {
+                    code: getQueryString('code')
+                }, (res) => {
+                    openid = res.data.openid;
+                    nickname = res.data.nickname;
+                    localStorage.openid = openid; // 存储用户id
+                    localStorage.nickname = nickname; // 存储用户名
+                });
+            }
+
+            // 获取奖品信息
+            $.get('/getAwards', {}, (res) => {
+                turnplate.restaraunts = res.result;
+                drawRouletteWheel();
+            });
+
+            // 获取已中奖信息
+            $.get('/getPrize', {}, (res) => {
+                $.each(res.result, (index, item) => {
+                    $('.swiper-wrapper').append(`<div class="swiper-slide">恭喜 <span style="color: #FBDB00;">${decodeURI(item.nickname)}</span> 抽到 <span style="color: #FBDB00;">${item.award_name}</span></div>`);
+                });
+                new Swiper ('.swiper-container', {
+                    autoplay: true,//可选选项，自动滑动
+                    speed: 700,
+                    height: 50,//你的slide高度
+                    direction : 'vertical',
+                    loop: true, // 循环模式选项
+                });        
+            });
+
+            // 获取初始化分享功能数据
+            let ticket = localStorage.getItem('ticket');
+            let appid = localStorage.getItem('appid');
+            let now_time = Date.parse(new Date()) / 1000;   //当前时间
+            let ticket_time = Number(localStorage.getItem('create_time')) + Number(localStorage.getItem('expires_in')); // 签名过期时间
+            let str = nonceStr();
+            if (ticket && appid && localStorage.getItem('create_time') && localStorage.getItem('expires_in') && now_time < ticket_time) {
+                wxShare(appid, str, ticket);
+            } else {
+                $.get('/getTrcket', {}, (res) => {
+                    localStorage.ticket = res.ticket;
+                    localStorage.appid = res.appid;
+                    localStorage.create_time = Date.parse(new Date()) / 1000;    // 设置时间
+                    localStorage.expires_in = res.expires_in;    // 过期时间
+                    wxShare(res.appid, str, res.ticket);
+                });
+            }
+        }
+    }
+
+    let wxShare = (appid, str, ticket) => {
+        //配置微信信息
+        wx.config({
+            debug: false, // true:调试时候弹窗
+            appId: appid, // 微信appid
+            timestamp: new Date().getSeconds(), // 时间戳
+            nonceStr: str, // 随机字符串
+            signature: create_signature(str, ticket, new Date().getSeconds(), window.location.href), // 签名
+            jsApiList: [
+                // 所有要调用的 API 都要加到这个列表中
+                'onMenuShareTimeline', // 分享到朋友圈接口
+                'onMenuShareAppMessage', //  分享到朋友接口
+                'onMenuShareQQ', // 分享到QQ接口
+                'onMenuShareWeibo' // 分享到微博接口
+            ]
+        });
+
+        wx.ready(function () {
+            // 微信分享的数据
+            var shareData = {
+                "imgUrl": 'http://suxiaozhi.tunnel.qydev.com/img/mn.gif', // 分享显示的缩略图地址
+                "link": 'http://suxiaozhi.tunnel.qydev.com/afk', // 分享地址
+                "desc": '为API生，为框架死，为debug奋斗一辈子，吃符号的亏，上大小写的当，最后死在需求上。', // 分享描述
+                "title": '码农的日常', // 分享标题
+                success: function () {
+                    // 分享成功可以做相应的数据处理
+                }
+            }
+            wx.onMenuShareTimeline(shareData);
+            wx.onMenuShareAppMessage(shareData);
+            wx.onMenuShareQQ(shareData);
+            wx.onMenuShareWeibo(shareData);
+        });
+    };
+
+    let nonceStr = () => {
+        let str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        let val = "";
+        for (let i = 0; i < 16; i++) {
+            val += str.substr(Math.round((Math.random() * 10)), 1);
+        }
+        return val;
+    }
+
+    let create_signature = (nocestr, ticket, timestamp, url) => sha1(`jsapi_ticket=${ticket}&noncestr=${nocestr}&timestamp=${timestamp}&url=${url}`);
+
+    let drawRouletteWheel = () => {
+        let canvas = document.getElementById("wheelcanvas");
+        if (canvas.getContext) {
+            //根据奖品个数计算圆周角度
+            let arc = Math.PI / (turnplate.restaraunts.length / 2);
+            let ctx = canvas.getContext("2d");
+            //在给定矩形内清空一个矩形
+            ctx.clearRect(0, 0, 516, 516);
+            //strokeStyle 属性设置或返回用于笔触的颜色、渐变或模式  
+            ctx.strokeStyle = "#FFBE04";
+            //font 属性设置或返回画布上文本内容的当前字体属性
+            ctx.font = 'bold 22px Microsoft YaHei';
+            for (let i = 0; i < turnplate.restaraunts.length; i++) {
+                let angle = turnplate.startAngle + i * arc;
+                ctx.fillStyle = turnplate.restaraunts[i].award_bg_color;
+                ctx.beginPath();
+                //arc(x,y,r,起始角,结束角,绘制方向) 方法创建弧/曲线（用于创建圆或部分圆）    
+                ctx.arc(258, 258, turnplate.outsideRadius, angle, angle + arc, false);
+                ctx.arc(258, 258, turnplate.insideRadius, angle + arc, angle, true);
+                ctx.stroke();
+                ctx.fill();
+                //锁画布(为了保存之前的画布状态)
+                ctx.save();
+
+                //----绘制奖品开始----
+                ctx.fillStyle = "#E83800";
+                //ctx.fillStyle = turnplate.fontcolors[i];
+                let text = turnplate.restaraunts[i].award_name;
+                let line_height = 30;
+                //translate方法重新映射画布上的 (0,0) 位置
+                ctx.translate(258 + Math.cos(angle + arc / 2) * turnplate.textRadius, 258 + Math.sin(angle + arc / 2) * turnplate.textRadius);
+
+                //rotate方法旋转当前的绘图
+                ctx.rotate(angle + arc / 2 + Math.PI / 2);
+                /** 下面代码根据奖品类型、奖品名称长度渲染不同效果，如字体、颜色、图片效果。(具体根据实际情况改变) **/
+                if (text.indexOf("\n") > 0) { //换行
+                    let texts = text.split("\n");
+                    for (let j = 0; j < texts.length; j++) {
+                        ctx.font = j == 0 ? '22px Microsoft YaHei' : '22px Microsoft YaHei';
+                        //ctx.fillStyle = j == 0?'#FFFFFF':'#FFFFFF';
+                        if (j == 0) {
+                            //ctx.fillText(texts[j]+"M", -ctx.measureText(texts[j]+"M").width / 2, j * line_height);
+                            ctx.fillText(texts[j], -ctx.measureText(texts[j]).width / 2, j * line_height);
+                        } else {
+                            ctx.fillText(texts[j], -ctx.measureText(texts[j]).width / 2, j * line_height);
+                        }
+                    }
+                } else if (text.indexOf("\n") == -1 && text.length > 6) { //奖品名称长度超过一定范围 
+                    text = text.substring(0, 6) + "||" + text.substring(6);
+                    let texts = text.split("||");
+                    for (let j = 0; j < texts.length; j++) {
+                        ctx.fillText(texts[j], -ctx.measureText(texts[j]).width / 2, j * line_height);
+                    }
+                } else {
+                    //在画布上绘制填色的文本。文本的默认颜色是黑色
+                    //measureText()方法返回包含一个对象，该对象包含以像素计的指定字体宽度
+                    ctx.fillText(text, -ctx.measureText(text).width / 2, 0);
+                }
+
+                //把当前画布返回（调整）到上一个save()状态之前 
+                ctx.restore();
+                mui.hideLoading(); //隐藏后的回调函数
+                //----绘制奖品结束----
+            }
+        }
+    }
+
+    let rnd = (n, m) => Math.floor(Math.random() * (m - n + 1) + n);
+
+    let dateFomart = () => {
+        const date = new Date();
+        let y = date.getFullYear().toString();
+        let m = (date.getMonth() + 1).toString();
+        let d = date.getDate().toString();
+        let h = date.getHours().toString();
+        let f = date.getMinutes().toString();
+        let s = date.getSeconds().toString();
+        return `${y}-${m.padStart(2, 0)}-${d.padStart(2, 0)} ${h.padStart(2, 0)}:${f.padStart(2, 0)}:${s.padStart(2, 0)}`;
+    }
+
+    let rotateFn = (item, data) => {
+        data.creat_time = dateFomart();
+        data.openid = openid;
+        data.nickname = encodeURI(nickname);
+        delete data.id;
+        delete data.award_bg_color;
+        $.get('/prizeProcess', {
+            data
+        }, (res) => {});
+        var angles = item * (360 / turnplate.restaraunts.length) - (360 / (turnplate.restaraunts.length * 2));
+        if (angles < 270) {
+            angles = 270 - angles;
+        } else {
+            angles = 360 - angles + 270;
+        }
+        $('#wheelcanvas').stopRotate();
+        $('#wheelcanvas').rotate({
+            angle: 0,
+            animateTo: angles + 1800,
+            duration: 6000,
+            callback: function () {
+                //中奖页面与谢谢参与页面弹窗
+                if (data.award_name.indexOf("谢谢参与") >= 0) {
+                    $(".xxcy_text").html(data.award_name);
+                    $("#xxcy-main").fadeIn();
+                    turnplate.bRotate = !turnplate.bRotate;
+                } else {
+                    $("#zj-main").fadeIn();
+                    var resultTxt = data.award_name.replace(/[\r\n]/g, ""); //去掉回车换行
+                    $("#jiangpin").text(resultTxt);
+                    turnplate.bRotate = !turnplate.bRotate;
+                }
+            }
+        });
+    };
+
+    init();
+
+    $('#tupBtn').click(() => {
+        if (turnplate.bRotate) return;
+
+        if (gameState == 0) {
+            $(".xxcy_text").html("活动时间：" + activityStart + " ~ " + activityEnd);
+            $("#xxcy-main").fadeIn();
+            return;
+        }
+
+        if (countMax <= count) {
+            $(".xxcy_text").html("抽奖次数已用完<br>分享可以增加一次抽奖机会！");
+            $("#xxcy-main").fadeIn();
+            return;
+        }
+        mui.showLoading("正在加载..", "div"); 
+        $.get('/getUserAward', {
+            openid: openid
+        }, (res) => {
+            mui.hideLoading();
+            if (res.result.length > 0) {
+                $(".xxcy_text").html('您好：' + decodeURI(res.result[0].nickname) + '，您于' + res.result[0].creat_time + '已参加本次活动，您本次活动的奖品为“' + res.result[0].award_name + '”，请尽快领取！');
+                $("#xxcy-main").fadeIn();
+            } else {
+                count++;
+                turnplate.bRotate = !turnplate.bRotate;
+                let item = rnd(0, turnplate.restaraunts.length - 1);
+                item == 0 ? turnplate.restaraunts.length : item;
+                rotateFn(item + 1, turnplate.restaraunts[item]);
+            }
+
+        });
+
+    });
+
+    $('.close_zj').click(() => {
+        $('#zj-main').fadeOut();
+    });
+
+    $('.close_xxcy').click(() => {
+        $('#xxcy-main').fadeOut();
+    });
+})
--- server/app.js
+++ server/app.js
@@ -1,73 +1,105 @@
-'use strict';
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+const request = require('request');
+const CONFIG = require('./config');
+const wechatApp = new WeChat();
 
-var express = require('express');
-var bodyParser = require('body-parser');
-var mysql = require('mysql');
 
-var hostName = '0.0.0.0'; // 设置主机名
-var port = 8090; // 设置端口号
-var appid = 'wx1f9c767629fabde5'; //appid
-var redirect_uri = 'http://47.110.73.210:8090/index.html';
-var connection = mysql.createConnection({
-    host: '114.115.234.194',
-    user: 'root',
-    password: '??8Hyq78q',
-    port: '3306',
-    database: 'lottery'
-});
+const SERVER = CONFIG.SERVER; // 设置服务器
+const WX = CONFIG.WX;   // 微信配置
+const MYSQL = CONFIG.MYSQL;
+
+// 数据库初始化、连接
+const connection = mysql.createConnection(MYSQL);
 connection.connect();
 
-var app = express();
+let app = express();
 app.use(bodyParser.urlencoded({
     extended: false
 }));
 
-app.use(express.static("./../"));
+app.use(express.static("./../")); //调用服务器文件夹下文件路径
 
-app.all('*', function (req, res, next) {
+let getQueryString = (search, name) => {
+    let reg = new RegExp(`(^|&)${name}=([^&]*)(&|$)`);
+    let r = search.substr(1).match(reg);
+    return r != null ? unescape(r[2]) : r;
+}
+
+app.all('*', (req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-    res.header("X-Powered-By", ' 3.2.1');
+    res.header("X-Powered-By", ' 3.2.1')
     res.header("Content-Type", "application/json;charset=utf-8");
     next();
 });
 
-// 保存用户信息
-app.get('/saveUserInfo', function (req, res) {
-    var userData = req.query.data;
-    console.log(userData);
-    var searchUserSql = 'select * from user_info where openid = \'' + userData.openid + '\'';
-    connection.query(searchUserSql, function (err, result) {
-        if (err) throw err;
-        if (result.length == 0) {
-            // user_info表中无此用户
-            var addUserInfoSql = 'insert into user_info set ?';
-            connection.query(addUserInfoSql, userData, function (addErr, addResult) {
-                if (addErr) throw addErr;
-                console.log('数据保存成功！');
-                res.send({
-                    flag: 1,
-                    desc: 'success！'
-                });
-            });
-        } else {
-            // user_info表中存在该用户
-            console.log('用户已存在！');
-            res.send({
-                flag: 0,
-                desc: '用户已存在！'
-            });
+// 访问微信获取获取用户信息
+app.get('/afk', (req, res) => {
+    res.redirect(`https://open.weixin.qq.com/connect/oauth2/authorize?appid=${WX.appid}&redirect_uri=${unescape(WX.redirect_uri)}&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect`);
+});
+
+// 跳转到主页面
+app.get('/afk1', (req, res) => {
+    let code = getQueryString(req._parsedUrl.search, 'code'); // 获取code
+    console.log(`code = ${code}`)
+
+    // console.log(code)
+    // res.redirect(`/index.html${req._parsedUrl.search}`);
+});
+
+app.get('/getUserInfo', (req, res) => {
+    let code = req.query.code;
+
+    // code 换取 access_token 和 openid
+    request(`https://api.weixin.qq.com/sns/oauth2/access_token?appid=${WX.appid}&secret=${WX.secret}&code=${code}&grant_type=authorization_code`, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            let access_token = JSON.parse(body).access_token;
+            let openid = JSON.parse(body).openid;
+            console.log(`access_token = ${access_token}`)
+            console.log(`openid = ${openid}`)
+
+            // access_token 和 openid 换取用户信息
+            request(`https://api.weixin.qq.com/sns/userinfo?access_token=${access_token}&openid=${openid}&lang=zh_CN`, function (error1, response1, body1) {
+                if (!error1 && response1.statusCode == 200) {
+                    let userData = JSON.parse(body1);
+                    console.log(userData) // Show the HTML for the baidu homepage.
+                    res.send({
+                        data: {
+                            nickname: userData.nickname,
+                            openid: userData.openid
+                        }
+                    });
+
+                    let searchUserSql = `select * from user_info where openid = '${openid}'`;
+                    connection.query(searchUserSql, (err, result) => {
+                        if (err) throw err;
+                        if (result.length == 0) {
+                            // user_info表中无此用户
+                            let addUserInfoSql = 'insert into user_info(openid, nickname, headimgurl, country, province, city, language, sex) value(?, ?, ?, ?, ?, ?, ?, ?)';
+                            let inserData = [userData.openid, encodeURI(userData.nickname), userData.headimgurl, userData.country, userData.province, userData.city, userData.language, userData.sex];
+                            connection.query(addUserInfoSql, inserData, (addErr, addResult) => {
+                                if (addErr) throw addErr;
+                                console.log('用户保存成功！');
+                            });
+                        } else {
+                            // user_info表中存在该用户
+                            console.log('用户已存在！');
+                        }
+                    });
+                }
+            })
         }
-    });
-    // connection.end();
+    })
 });
 
 // 获取奖品数据信息
-app.get('/getAwards', function (req, res) {
-    var searchAwardsSql = 'select * from award_info';
-    var flag = void 0;
-    connection.query(searchAwardsSql, function (err, result) {
+app.get('/getAwards', (req, res) => {
+    let searchAwardsSql = 'select * from award_info';
+    let flag;
+    connection.query(searchAwardsSql, (err, result) => {
         if (err) throw err;
         flag = result.length > 0 ? 1 : 0;
         res.send({
@@ -77,10 +109,10 @@ app.get('/getAwards', function (req, res) {
     });
 });
 
-//查询用户历史中奖奖品
-app.get('/getUserAward', function (req, res) {
-    var searchUserPrizeInfoSql = 'select nickname, award_name, creat_time from prize_info where openid = \'' + req.query.openid + '\' and is_get = \'0\'';
-    connection.query(searchUserPrizeInfoSql, function (prizeErr, prizeAddResult) {
+// 查询用户历史中奖奖品
+app.get('/getUserAward', (req, res) => {
+    let searchUserPrizeInfoSql = `select nickname, award_name, creat_time from prize_info where openid = '${req.query.openid}' and is_get = '0'`;
+    connection.query(searchUserPrizeInfoSql, (prizeErr, prizeAddResult) => {
         if (prizeErr) throw prizeErr;
         console.log(prizeAddResult);
         res.send({
@@ -91,15 +123,34 @@ app.get('/getUserAward', function (req, res) {
     });
 });
 
+
+// 获取初始化分享字段
+app.get('/getTrcket', (req, res) => {
+    request(`https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${WX.appid}&secret=362855efacd523cedc6bc18805bb74b6`, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            request(`https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=${JSON.parse(body).access_token}&type=jsapi`, function (error1, response1, body1) {
+                if (!error1 && response1.statusCode == 200) {
+                    console.log(JSON.parse(body1))
+                    res.send({
+                        ticket: JSON.parse(body1).ticket,
+                        appid: WX.appid,
+                        expires_in: JSON.parse(body1).expires_in
+                    });
+                }
+            });
+        }
+    });
+});
+
 // 中奖数据入库
-app.get('/prizeProcess', function (req, res) {
-    console.log(req.query.data);
+app.get('/prizeProcess', (req, res) => {
+    console.log(req.query.data)
     req.query.data.is_get = '0';
-    var prizeProcessSql = 'insert into prize_info set ?';
-    var flag = void 0;
-    connection.query(prizeProcessSql, req.query.data, function (err, result) {
+    let prizeProcessSql = 'insert into prize_info set ?';
+    let flag;
+    connection.query(prizeProcessSql, req.query.data, (err, result) => {
         if (err) throw err;
-        console.log(result);
+        console.log(result)
         flag = result.length > 0 ? 1 : 0;
         res.send({
             flag: flag,
@@ -108,20 +159,74 @@ app.get('/prizeProcess', function (req, res) {
     });
 });
 
-// 访问微信获取获取用户信息
-app.get('/weixin', function (req, res) {
-    res.redirect('https://open.weixin.qq.com/connect/oauth2/authorize?appid=' + appid + '&redirect_uri=' + unescape(redirect_uri) + '&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect');
+// 获取已中奖用户信息
+app.get('/getPrize', (req, res) => {
+    let getPrizeSql = `select nickname, award_name from prize_info where award_id != '00000' limit 10`;
+    connection.query(getPrizeSql, (err, result) => {
+        if (err) throw err;
+        res.send({
+            result: result
+        })
+    });
 });
 
-app.post("/post", function (req, res) {
+// 后台管理登录
+app.post("/loginVerify", (req, res) => {
     console.log("请求参数：", req.body);
-    var result = {
-        code: 200,
-        msg: "post请求成功"
-    };
-    res.send(result);
+    // res.send(result);
+    connection.query(`select * from admin where user_name = '${req.body.userName}'`, (err, result) => {
+        if (err) throw err;
+        if(result.length > 0) {
+            connection.query(`select * from admin where user_name = '${req.body.userName}' and user_password = '${req.body.password}'`, (err1, result1) => {
+                if (err1) throw err1;
+                if(result1.length > 0) {
+                    res.send({
+                        flag: 1,
+                        desc: '登录成功！'
+                    });
+                }else {
+                    res.send({
+                        flag: 0,
+                        desc: '密码错误！'
+                    });
+                }
+            });
+        }else {
+            res.send({
+                flag: 0,
+                desc: '用户不存在！'
+            });
+        }
+    });
 });
 
-app.listen(port, hostName, function () {
-    console.log('\u670D\u52A1\u5668\u8FD0\u884C\u5728http://' + hostName + ':' + port);
+// 查询所有奖品信息
+app.get('/getAllPrize', (req, res) => {
+    connection.query(`select * from prize_info, user_info where prize_info.openid = user_info.openid order by creat_time limit ${(req.query.page - 1) * req.query.limit}, ${req.query.limit}`, (err, result) => {
+        if (err) throw err;
+        connection.query(`select * from prize_info`, (err1, result1) => {
+            if (err1) throw err1;
+            res.send({
+                code: 0,
+                count: result1.length,
+                data: result
+            });
+        });
+    });
+}); 
+
+// 更新是否领取字段状态值
+app.get('/updataIsGet', (req, res) => {
+    connection.query(`update prize_info set is_get = '${req.query.state}' where openid = '${req.query.openid}' and creat_time = '${req.query.creat_time}'`, (err, result) => {
+        if (err) throw err;
+        res.send({
+            flag: 1,
+            desc: 'success'
+        });
+    });
 });
+
+app.listen(SERVER.port, SERVER.hostName, () => {
+    console.log(`服务器运行在http://${SERVER.hostName}:${SERVER.port}`);
+});
+
